3224229447ff9e82eccd4be8bac716956a39a3fd
simpleithub <md.saigeetha@gmail.com>
2024-02-21 20:17:54 +0530

    sum of 2 nos.

---
+++ sum2Nos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+int n1 ,n2,sum ;
+
+printf("enter no1 - ");
+scanf("%d",&n1);
+
+printf("enter no2 - ");
+scanf("%d",&n2);
+
+sum = n1+n2;
+printf("the sum is %d - ",sum);
+
+return 0;
+}
+
+
+
